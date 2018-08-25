aa77505bce985a3a4ec6f1bfa27d96c56bb2e70f
Аlexey Medvedev <medva1997@yandex.ru>
2018-08-25 19:36:02 +0300

    Update postinstall18.sh

--- postinstall18.sh
+++ postinstall18.sh
@@ -28,3 +28,41 @@ sudo snap install datagrip --classic
 sudo snap install clion --classic
 
 sudo snap install obs-studio telegram-desktop keepassxc
+
+
+
+cat <<EOF >> ~/.bashrc
+
+alias sshsee='ssh alexey@109.120.138.81'
+alias sshusa='ssh alexey@207.244.96.195'
+alias sshmail=''
+alias sshpi='ssh pi@192.168.1.17'
+alias sshserver0='ssh alexey@192.168.1.23'
+alias sshserver1='ssh alexey@192.168.1.12'
+
+alias sshmik1='ssh admin@192.168.1.1'
+alias sshmik2='ssh admin@192.168.1.202'
+alias sshmik3='ssh admin@192.168.1.203'
+alias sshmik4='ssh admin@192.168.1.204'
+alias sshmik5='ssh admin@192.168.1.205'
+alias sshmik6='ssh admin@192.168.1.206'
+alias sshmik7='ssh admin@192.168.1.207'
+alias sshmik8='ssh admin@192.168.1.208'
+
+alias push='git push'
+alias pull='git pull'
+alias clone='git clone'
+alias commit='git commit -m'
+alias tmu='tmux attach || tmux new'
+
+ssh() {
+    if [ "$(ps -p $(ps -p $$ -o ppid=) -o comm=)" = "tmux" ]; then
+            tmux rename-window "\$*"
+            command ssh "\$@"
+            tmux set-window-option automatic-rename "on" 1>/dev/null
+    else
+            command ssh "\$@"
+    fi
+}
+
+EOF
